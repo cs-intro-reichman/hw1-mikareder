0a19952ff2bd08cddc981ee6d5ee719b9a7c032f
Mika Reder <mikareder@Mikas-MacBook-Air.local>
2025-10-30 22:43:50 +0200

    hw1

--- Bill3.java
+++ Bill3.java
@@ -5,8 +5,8 @@ public class Bill3 {
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int value = Integer.parseInt(args[3]);
-		double amount = value/3;
-		System.out.println("Dear " + name1 + ", " +name2 + " and " + name3 + ": pay "+ amount + "Shekels each.");
+		double value = Double.parseDouble(args[3]);
+		double amount = Math.ceil(value/3);
+		System.out.println("Dear " + name1 + ", " +name2 + " and " + name3 + ": pay "+ amount + " Shekels each.");
 	}
 }
